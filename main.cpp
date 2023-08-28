2092270abc9b4ca4e46751bf2cbc9c5a22934612
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 17:30:57 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  int num1, num2, num3, num4, num5;
+  double total;
+  double average;
+
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << "Total sum: " << total << endl;
+  cout << setprecision(2) << fixed;
+  cout << "Average: " << average << endl;
+}
